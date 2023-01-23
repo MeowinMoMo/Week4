8558194bd298839bb4221e90e494272f3306310a
Twin29 <christa.ramos@ciit.edu.ph>
2023-01-23 18:42:05 +0800

    Activity 4

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private AudioSource PewPewPew;
+    public Camera sceneCamera;
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+    public Weapon weapon;
+
+    private Vector2 moveDirection;
+    private Vector2 mousePosition;
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+            PewPewPew.Play();
+        }
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+    }
+}
